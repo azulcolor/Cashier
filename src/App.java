1b161f1c32f49829e0c83d058ad90e936e41e27e
Pastelin <lealpastelin@gmail.com>
2022-09-06 00:16:33 -0500

    Adding all the created files

---
+++ src/App.java
@@ -0,0 +1,69 @@
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        int opcion;
+        int dinero;
+        boolean cuentaCreada;
+        Cajero cajero = new Cajero();
+        
+        do {
+                opcion = cajero.funciones();
+                cuentaCreada = cajero.getCuentaCreada();
+                switch (opcion) {
+                    case 1:
+                        if ( cuentaCreada == false) {
+                            cajero.crearCuenta();
+                            System.out.println("\nCuenta creada con éxito\n");
+                        } else {
+                            System.out.println("\nYa tienes una cuenta creada\n");
+                        }
+                            break;
+                         // Crear cuenta vacía
+                        
+                    case 2: // Crear cuenta saldo inicial
+                        if ( cuentaCreada == false) {
+                            System.out.print("\nIntroduce el saldo inicial: $");
+                            dinero = Integer.parseInt(System.console().readLine());
+                            cajero.crearCuenta(dinero);
+                        } else {
+                            System.out.print("\nYa tienes una cuenta creada");
+                        }
+                        break;
+
+                    case 3: // Ingresar dinero
+                        if ( cuentaCreada == true) {
+                            System.out.print("\nIntroduce la cantidad a ingresar: $");
+                            dinero = Integer.parseInt(System.console().readLine());
+                            cajero.ingresar(dinero);
+                        } else {
+                            System.out.print("\nNo tienes una cuenta creada\n");
+                        }
+                        break;
+
+                    case 4: // Retirar dinero
+                        if ( cuentaCreada == true) {
+                            System.out.print("\nIntroduce la cantidad a retirar: $");
+                            dinero = Integer.parseInt(System.console().readLine());
+                            cajero.retirar(dinero);                      
+                        } else {
+                            System.out.print("\nNo tienes una cuenta creada\n");
+                        }
+                        break;
+
+                    case 5: // Ver saldo 
+                        if ( cuentaCreada == true) {
+                            System.out.print("\nTu saldo es de: $" + cajero.getSaldo() + "\n");
+                        } else {
+                            System.out.print("\nNo tienes una cuenta creada\n");
+                        }
+                        break;
+
+                    case 6: // Salir 
+                        System.out.println("\nGracias por su preferencia");
+                        break;
+                }
+        } while (opcion != 6);
+
+
+    }
+}
---
+++ src/Cajero.java
@@ -0,0 +1,69 @@
+public class Cajero {
+    float dinero;
+    boolean cuentaCreada = false;
+    int opcion;
+
+    public Cajero() {
+    }
+
+    public void crearCuenta() {
+        this.cuentaCreada = true;
+    }
+
+    public void crearCuenta(float dinero) {
+        if (dinero > 0) {
+            this.dinero = dinero;
+            this.cuentaCreada = true;
+            System.out.println("\nCuenta creada con saldo de " + dinero +  " fue creada con éxito\n");
+        } else {
+            System.out.println("No se puede crear una cuenta con saldo negativo");
+        }
+    }
+
+    public void ingresar(int dinero) {
+        if (dinero > 0) {
+            this.dinero += dinero;
+            System.out.print("\nHas ingresado $" + dinero + " en tu cuenta\n");
+        } else {
+            System.out.println("No se puede ingresar una cantidad negativa");
+        }
+    }
+
+    public void retirar(int dinero) {
+        if (dinero > this.dinero) {
+            System.out.print("\nNo tienes suficiente dinero en tu cuenta\n");
+        } else if(dinero < 0) {
+            System.out.println("No se puede retirar una cantidad negativa");
+        } else {
+            this.dinero -= dinero;
+            System.out.print("\nHas retirado $" + dinero + " de tu cuenta\n");
+        }
+    }
+
+    public float getSaldo() {
+        return dinero;
+    }
+
+    public boolean getCuentaCreada() {
+        return cuentaCreada;
+    }
+
+    public int funciones() {
+
+        if (opcion == 0) {
+            System.out.println("\nBienvenido al cajero");
+        }
+
+        System.out.println("\n1.- Crear cuenta vacía");
+        System.out.println("2.- Crear cuenta saldo inicial");
+        System.out.println("3.- Ingresar dinero");
+        System.out.println("4.- Retirar dinero");
+        System.out.println("5.- Ver saldo");
+        System.out.println("6.- Salir");
+        System.out.print("Elige una opción: ");
+
+        opcion = Integer.parseInt(System.console().readLine());
+
+        return opcion;
+    }
+}
